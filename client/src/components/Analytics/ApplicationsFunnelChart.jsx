95c7238035df527be20984c19ce9d1e5c580c60c
jerrysummer <reason2be@gmail.com>
2017-09-24 23:59:29 -0700

    added funnel chart in analytics

--- client/src/components/Analytics/ApplicationsFunnelChart.jsx
+++ client/src/components/Analytics/ApplicationsFunnelChart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './../../../styles/socialGraphStyles.css'
+import styles from './../../../styles/jerryStyleBox.css'
 import PropTypes from 'prop-types'
 import D3Funnel from 'd3-funnel';
 import axios from 'axios'
@@ -20,28 +20,54 @@ export default class ApplicationsFunnelChart extends React.Component{
       { label: 'On Site ', value: 10 },
       { label: 'Offer', value: 2 },
     ];
-    const options = {
-      block:{
-        dynamicHeight: true,
-        minHeight: 15,
-      },
-    };
+
     axios.get('/api/applications')
       .then((applications) => {
         let stages = []
         applications.data.forEach((application) => stages.push(application.stage));
         return stages
       })
-      .then((data) => console.log(data))
+      .then((data) => {
+        let storage = {};
+        data.forEach((stage) => {
+          storage[stage] = storage[stage] + 1 || 1;
+        })
+        return storage;
+      })
+      .then((data) => {
+        let converted = [];
+        for(let key in data) {
+          let temp = {};
+          temp['label'] = key;
+          temp['value'] = data[key];
+          converted.push(temp);
+        }
+        return converted
+      })
+      .then((data) => {
+        const fakeData = [
+          { label: 'Applied', value: 300 },
+          { label: 'Phone Screen', value: 50 },
+          { label: 'Tech Screen', value: 10 },
+          { label: 'On Site ', value: 3 },
+          { label: 'Offer', value: 1 },
+        ];
+        const options = {
+          block:{
+            dynamicHeight: true,
+            minHeight: 15,
+          },
+        };
+        const chart = new D3Funnel('#funnel');
+        chart.draw(data, options);
+      })
 
-    const chart = new D3Funnel('#funnel');
-    chart.draw(data, options);
   }
   render() {
     return (
       <div>
-        <div id="funnel"></div>
-        <h1>TEST MESSAGE</h1>
+        <div id="funnel">
+        </div>
       </div>
     )
   }
